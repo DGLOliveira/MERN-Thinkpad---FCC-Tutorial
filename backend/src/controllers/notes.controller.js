1a384c33a44d4d3d8b80d9e89c3afedde957acea
DGLOliveira <dgloliveira90@gmail.com>
2026-04-07 16:19:14 +0000

    Installed nodemon; Set up routes and controllers; Optimized files and folders structure.

---
+++ backend/src/controllers/notes.controller.js
@@ -0,0 +1,15 @@
+export const getAllNotes = (req, res) => {
+    res.status(200).send("You just fetched all notes");
+}
+
+export const createNote = (req, res) => {
+    res.status(201).send("You just created a note");
+}
+
+export const updateNote = (req, res) => {
+    res.status(200).send("You just updated a note");
+}
+
+export const deleteNote = (req, res) => {
+    res.status(201).send("You just deleted a note");
+}
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,11 @@
 import extress from "express";
-
+import notesRoutes from "./routes/notes.routes.js";
 
 const app = extress();
 
-app.get("/api/hello", (req, res) => {
-    res.send("hello from the server");
-});
+app.use("/api/notes", notesRoutes);
+
+
 
 app.listen(5000, () =>{ 
     console.log("server is running");
