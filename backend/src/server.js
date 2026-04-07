507c9acbbec25321ce9fd37f3d47df640bb075b8
DGLOliveira <dgloliveira90@gmail.com>
2026-04-07 17:17:34 +0000

    Setup Database

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,18 @@
 import extress from "express";
 import notesRoutes from "./routes/notes.routes.js";
+import { connectDB } from "./config/db.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = extress();
+
+connectDB();
 
 app.use("/api/notes", notesRoutes);
 
 
 
-app.listen(5000, () =>{ 
-    console.log("server is running");
+app.listen(process.env.PORT, () =>{ 
+    console.log("server is running at PORT", process.env.PORT);
 });
