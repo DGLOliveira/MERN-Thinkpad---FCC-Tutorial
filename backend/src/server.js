f536c963ee839b4fee757440b973a4e19c339bdc
DGLOliveira <dgloliveira90@gmail.com>
2026-04-09 15:31:25 +0000

    Prepared for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path";
 
 import notesRoutes from "./routes/notes.routes.js";
 import { connectDB } from "./config/db.js";
@@ -9,20 +10,30 @@ import rateLimiter from "./middleware/rateLimiter.js";
 dotenv.config();
 
 const app = express();
-
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
-//middleware
-app.use(cors({
-    origin:"http://localhost:5173"
-}));
+//middlewares
+if (!process.env.NODE_ENV === "production") {
+    app.use(cors({
+        origin: "http://localhost:5173"
+    }));
+}
 app.use(express.json());
 app.use(rateLimiter);
 
-
 app.use("/api/notes", notesRoutes);
 
-connectDB().then(() =>{
+if (process.env.NODE_ENV === "production") {
+    //Starts frontend
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+    //Redirects any path other than the previous to the frontend
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+    })
+}
+
+connectDB().then(() => {
     app.listen(PORT, () => {
         console.log("server is running at PORT", PORT);
     });
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
+const baseURL = import.meta.env.MODE === "development" ? "http://localhost:5000/api/" : "/api";
 export const api = axios.create({
     baseURL: "http://localhost:5000/api/",
 });
